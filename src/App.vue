1dc3908b3de737d1b9b0e53ebf9e6252da6d1a3b
mikepxq <1024694765@qq.com>
2021-11-25 01:00:12 +0800

    feat: update App.vue to setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
+<script lang="ts">
+export default { name: "App" };
+</script>
 <template>
   <router-view class="m-page"></router-view>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-  name: "App",
-});
-</script>
